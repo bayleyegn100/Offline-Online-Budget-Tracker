9c800c1ff5b2dfd78ea4f7a1e5b850211fcfd060
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-03 23:42:07 -0700

    check and declare the versions, creat budget store

--- public/db.js
+++ public/db.js
@@ -3,3 +3,16 @@ let db;
 let budgetVersion;
 // creat a database request in indexdb
 const request = indexedDB.open("BudgetDB", budgetVersion || 21);
+// checks the version to check the indexdb needs update or not.
+request.onupgradeneeded = function (e) {
+    console.log("Upgrade needed in IndexDB");
+    // declaring both versions of the dbs
+    const { oldVersion } = e;
+    const newVersion = e.newVersion || db.version;
+    console.log(`Database Updated from version ${oldVersion} to ${newVersion}`);
+    db = e.target.resutl;
+    // crating a budget store if not avilable.
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore('BudgetStore', { autoIncrement: true });
+      }
+    };
