382f5c9fec1d035e64e60760d475ec663579dcbd
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-06 17:40:06 -0700

    deploying

--- server.js
+++ server.js
@@ -15,8 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.NONGOGB_URI || "mongodb://localhost/budget", {
+mongoose.connect(
+  process.env.NONGOGB_URI || 
+  "mongodb://localhost/budget", 
+  {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
