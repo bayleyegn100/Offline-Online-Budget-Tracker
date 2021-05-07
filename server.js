e7b65fc228e56f296e15ae8f6cc080208ee8ae52
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-06 18:00:48 -0700

    corrected spelling error

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.NONGOGB_URI || 
+  process.env.MONGOGB_URI || 
   "mongodb://localhost/budget", 
   {
   useNewUrlParser: true,
