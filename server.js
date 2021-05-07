62e071c96d37d25e434406da0070b00af5bba658
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-06 18:13:00 -0700

    final push

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 // use port 3000 unless there exists a preconfigured port
-const PORT = process.env.Port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGOGB_URI || 
+  process.env.MONGODB_URI || 
   "mongodb://localhost/budget", 
   {
   useNewUrlParser: true,
